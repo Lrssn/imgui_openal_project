43890081fad7ca3c82b63afe07ad11c14c27740e
Lrssn <lrssn95@gmail.com>
2018-06-14 14:13:50 +0100

    now its on the right version

--- imgui_openal_project/Main.cpp
+++ imgui_openal_project/Main.cpp
@@ -10,24 +10,14 @@ int main(int argc, char** argv) {
 	Window window(height, width);
 	window.update();
 
-<<<<<<< HEAD
-	// Play
-	alSourcePlay(source1);
-	alSourcePlay(source2);
-	// Wait for the song to complete
-	do {
-		alGetSourcei(source2, AL_SOURCE_STATE, &state);
-	} while (state == AL_PLAYING);
-	std::cout << "played sound" << std::endl;
-=======
 	//audio
 	AudioManager am;
 	AudioSource As("./res/audio/bounce.wav");
->>>>>>> 7a376c2d7fe38fb4a971d3ee06ae9c0878d457e9
+
 
 	am.setVolume(1.5);
 	am.Play(&As);
-	As.setLooping(true);
+	As.setLooping(false);
 	As.setPitch(2.0);
 	am.Play(&As);
 	As.~AudioSource();
